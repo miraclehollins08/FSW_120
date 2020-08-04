58e1d9d41f47b1fcae5e0f1b43d8bb2bfc8983ca
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-08-04 13:35:58 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App() {
+return (
+    <div>
+
+        <h1>ToDo Part1</h1>
+        
+        <input type="checkbox" />
+        <p>Install React App</p>
+
+        <input type="checkbox" />
+        <p>create Component folder</p>
+
+        <input type="checkbox" />
+        <p>watch the react todo video</p>
+
+        <input type="checkbox" />
+        <p>Practice react by yourself</p>
+    </div>
+)
+
+}
+
+export default App;
